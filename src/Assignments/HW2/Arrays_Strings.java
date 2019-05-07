9614c6de24e29b40db5b4294b0d5267ff5f5c1da
Brianna <blr3@illinois.edu>
2019-05-07 17:41:12 -0500

    pairsThatSum method first iteration commit

--- src/Assignments/HW2/Arrays_Strings.java
+++ src/Assignments/HW2/Arrays_Strings.java
@@ -14,18 +14,18 @@ public class Arrays_Strings {
      *
      *
      * Implement the function: Array<Pair<int>> pairsThatEqualSum(Array<int> inputArray, int targetSum)
-     * ○This function takes an array of integers and a target integer to which the array elements must sum.
+     * This function takes an array of integers and a target integer to which the array elements must sum.
      * It returns an array of all pairs of integers from the input array whose sum equals the target.
      * */
 
     public static boolean isStringPermutation(String s1, String s2) {
-        if (s1 != null && s2 != null & s1.length() == s2.length()) {
+        if (s1 != null && s2 != null & (s1.length() == s2.length())) {
             HashMap<Character, Integer> s1_map = new HashMap<>();
             HashMap<Character, Integer> s2_map = new HashMap<>();
 
             for (int i = 0; i < s1.length(); i++) {
                 s1_map.put(s1.charAt(i), s1_map.getOrDefault(s1.charAt(i),0) + 1);
-                s2_map.put(s2.charAt(i), s2_map.getOrDefault(s2.charAt(i), 0) + 1);
+                s2_map.put(s2.charAt(i), s2_map.getOrDefault(s2.charAt(i),0) + 1);
             }
 
             return s1_map.equals(s2_map);
@@ -36,28 +36,50 @@ public class Arrays_Strings {
 
     public static boolean isStringPermutationIter1(String s1, String s2) {
         if (s1 != null && s2 != null & s1.length() == s2.length()) {
-            int i = 0;
             List<Character> s1_list = new ArrayList<>();
 
             for (Character letter : s1.toCharArray()) {
                 s1_list.add(letter);
             }
 
-            while (i < s2.length()) {
-                if (s1_list.contains(s2.charAt(i))) {
-                    s1_list.remove(Character.valueOf(s2.charAt(i)));
-                }
-                i++;
+            for (Character character : s2.toCharArray()) {
+                s1_list.remove(character);
             }
 
             return s1_list.size() == 0;
         }
+
         return false;
+    }
+
+    public static ArrayList<int[]> pairsThatEqualSum(int[] inputArray, int targetSum) {
+        if (inputArray != null) {
+            ArrayList<int[]> arrayToReturn = new ArrayList<>();
+            Arrays.sort(inputArray);
+
+            for (Integer num : inputArray) {
+                if (arrayToReturn.size() < inputArray.length/2) {
+                    int target = targetSum - num;
+                    int found = Arrays.binarySearch(inputArray, target);
+
+                    if (found > -1) {
+                        int[] pair = new int[]{num, inputArray[found]};
+                        arrayToReturn.add(pair);
+                    }
+                }
+            }
+            return arrayToReturn;
+        }
+
+        return null;
     }
 
     public static void main(String[] args) {
 
-        boolean ans = isStringPermutationIter1("abs", "cda");
+        int[] test = new int[] {1,2,3,4};
+        System.out.println(pairsThatEqualSum(test,5));
+
+        boolean ans = isStringPermutationIter1("abs", "asb");
         System.out.println(ans);
 
         boolean ans1 = isStringPermutation("abs", "sba");
