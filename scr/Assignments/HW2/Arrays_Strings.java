151fc91d0eae5b900eb9b2e315084e3cf9bdee85
Brianna <blr3@illinois.edu>
2019-05-06 23:41:39 -0500

    hw1 & hw2 folders

---
+++ scr/Assignments/HW2/Arrays_Strings.java
@@ -0,0 +1,12 @@
+package Assignments.HW2;
+
+public class Arrays_Strings {
+    /**
+     * Implement the function: Boolean isStringPermutation(String s1, String s2)
+     * This function takes two strings and returns true if one string is a permutation of the other, false otherwise.
+     *
+     * Implement the function: Array<Pair<int>> pairsThatEqualSum(Array<int> inputArray, int targetSum)
+     * ○This function takes an array of integers and a target integer to which the array elements must sum.
+     * It returns an array of all pairs of integers from the input array whose sum equals the target.
+     * */
+}
