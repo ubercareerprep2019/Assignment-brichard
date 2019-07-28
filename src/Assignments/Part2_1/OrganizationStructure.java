02a5a7834f3587323fab0d8541661967002e182f
Brianna <blr3@illinois.edu>
2019-07-27 23:58:05 -0700

    HW2 part 1 ex 2 and 3 completed

--- src/Assignments/Part2_1/OrganizationStructure.java
+++ src/Assignments/Part2_1/OrganizationStructure.java
@@ -36,38 +36,112 @@ public class OrganizationStructure {
         public List<Employee> directReports;
     }
 
-    Queue<Employee> queue = new LinkedList<>();
+    private Queue<Employee> queue = new LinkedList<>();
 
-    public void printLevelByLevel(Employee employee) {
-         // need to take out arguments
-         printLevelByLevelHelper(employee);
+    public void printLevelByLevel() {
+        queue.add(this.ceo);
+
+        while (!queue.isEmpty()) {
+            Queue<Employee> temp = new LinkedList<>();
+            temp.addAll(queue);
+
+            for (Employee person : queue) {
+                System.out.println("Name: " + person.name + ", Title: " + person.title);
+            }
+
+            System.out.println();
+            queue.clear();
+
+            for (Employee emp : temp) {
+                if (emp.directReports != null) {
+                    queue.addAll(emp.directReports);
+                }
+            }
+        }
     }
 
-    public void printLevelByLevelHelper(Employee employee) {
-         if (queue.isEmpty()) {
-             queue.add(employee);
-         }
+    public int printNumLevels() {
+        int numLevels = 0;
+        queue.add(this.ceo);
 
-         for (Employee person : queue) {
-             System.out.println("Name: " + person.name + ", Title: " + person.title);
-         }
 
-         System.out.println();
+        while (!queue.isEmpty()) {
+            Queue<Employee> temp = new LinkedList<>();
+            temp.addAll(queue);
+            queue.clear();
+            numLevels++;
 
-         queue.addAll(queue.remove().directReports);
-         printLevelByLevelHelper(queue.peek());
+            for (Employee emp : temp) {
+                if (emp.directReports != null) {
+                    queue.addAll(emp.directReports);
+                }
+            }
+        }
+
+        return numLevels;
     }
 
     public static void main(String[] args) {
          OrganizationStructure org = new OrganizationStructure();
 
+        OrganizationStructure.Employee k = new OrganizationStructure.Employee();
+        k.name = "K";
+        k.title = "Sales Intern";
+
+        OrganizationStructure.Employee h = new OrganizationStructure.Employee();
+        h.name = "H";
+        h.title = "Engineer";
+
+        OrganizationStructure.Employee g = new OrganizationStructure.Employee();
+        g.name = "G";
+        g.title = "Engineer";
+
+        OrganizationStructure.Employee f = new OrganizationStructure.Employee();
+        f.name = "F";
+        f.title = "Engineer";
+
+        OrganizationStructure.Employee sr = new OrganizationStructure.Employee();
+        sr.name = "J";
+        sr.title = "Sales Representative";
+        List<Employee> list1 = new ArrayList<>();
+        list1.add(k);
+        sr.directReports = list1;
+
+        OrganizationStructure.Employee Manager2 = new OrganizationStructure.Employee();
+        Manager2.name = "E";
+        Manager2.title = "Manager";
+
+        OrganizationStructure.Employee d = new OrganizationStructure.Employee();
+        d.name = "D";
+        d.title = "Manager";
+        List<Employee> lis5 = new ArrayList<>();
+        lis5.add(f);
+        lis5.add(g);
+        lis5.add(h);
+        d.directReports = lis5;
+
+
+         OrganizationStructure.Employee Director = new OrganizationStructure.Employee();
+         Director.name = "I";
+         Director.title = "Director";
+        List<Employee> lis4 = new ArrayList<>();
+        lis4.add(sr);
+        Director.directReports = lis4;
+
          OrganizationStructure.Employee cfo = new OrganizationStructure.Employee();
          cfo.name = "B";
          cfo.title = "CFO";
+         List<Employee> lis2 = new ArrayList<>();
+         lis2.add(Director);
+         cfo.directReports = lis2;
 
          OrganizationStructure.Employee cto = new OrganizationStructure.Employee();
          cto.name = "C";
          cto.title = "CTO";
+         List<Employee> lis3 = new ArrayList<>();
+         lis3.add(d);
+         lis3.add(Manager2);
+         cto.directReports = lis3;
 
          OrganizationStructure.Employee ceo = new OrganizationStructure.Employee();
          ceo.name = "A";
@@ -78,7 +152,8 @@ public class OrganizationStructure {
          ceo.directReports = list;
          org.ceo = ceo;
 
-         org.printLevelByLevel(ceo);
+        org.printLevelByLevel();
+        System.out.println(org.printNumLevels());
 
     }
 }
